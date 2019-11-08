a98bbbb906b95b5cdf46f30d302b7d216c3a1bd7
Stuart Runyan <shrunyan@gmail.com>
2019-11-08 15:08:33 -0800

    added: init command to setup CLI configurations

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,25 +1,105 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from "@oclif/command";
+import * as notifier from 'node-notifier'
+import cli from 'cli-ux'
+
+const os = require("os");
+const fs = require("fs");
+const path = require("path");
+const execa = require("execa");
+const Listr = require("listr");
+const chalk = require("chalk");
 
 export default class Init extends Command {
-  static description = 'describe the command here'
+  static description = "describe the command here";
 
   static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+    help: flags.help({ char: "h" })
+  };
 
-  static args = [{name: 'file'}]
+  static args = [{ name: "zuid" }];
 
   async run() {
-    const {args, flags} = this.parse(Init)
+    const { args } = this.parse(Init);
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /home/shrunyan/repos/zesty-io/cli/src/commands/init.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    const tasks = new Listr([
+      {
+        title: "Create CLI settings directory",
+        task: () => {
+          try {
+            // Make config dir
+            fs.mkdir(this.config.configDir, {
+              recursive: true
+            }, err => {
+              if (err && err.code !== "EEXIST") {
+                throw err
+              }
+
+              // If zuid was provided create dir for that instance and setup config file
+              if (args.zuid) {
+                const instancedir = path.resolve(this.config.configDir, args.zuid);
+                fs.mkdir(instancedir, err => {
+                  if (err && err.code !== "EEXIST") {
+                    throw err
+                  }
+
+                  // Generate config file
+                  fs.writeFile(
+                    path.resolve(instancedir, "config.json"),
+                    `{"INSTANCE_ZUID: "${args.zuid}"}`,
+                    err => {
+                      if (err && err.code !== "EEXIST") {
+                        throw err
+                      }
+                      // console.log("Generated config file");
+                    }
+                  );
+                });
+              } else {
+                if (err && err.code === "EEXIST") {
+                  console.log(chalk.yellow(`The CLI has already been initialized. Would you like to initalize a specific instance? Try using zesty init ZUID`))
+                }
+              }
+            });
+          } catch (err) {
+            console.error(err);
+          }
+        }
+      },
+      {
+        title: `Authenticating with ${args.zuid} instance`,
+        enabled: () => args.zuid,
+        task: async () => {
+
+          const email = await cli.prompt('Enter your Zesty.io user account email')
+          const pass = await cli.prompt('Enter your Zesty.io user account password')
+
+          execa(`zesty auth ${email} ${pass}`)
+            .catch((err) => {
+              console.error(err)
+              throw err
+            })
+        }
+      },
+      {
+        title: 'Pulling Instance Files',
+        enabled: () => args.zuid,
+        task: () => {
+          // TODO Pull instances files
+        }
+      },
+      {
+        title: 'Done!',
+        task: () => {
+          notifier.notify({
+            title: 'Zesty.io CLI',
+            message: args.zuid ? `Initialized with instance ${args.zuid}` : `Initialized`
+          })
+        }
+      }
+    ], {concurrent: false});
+
+    tasks.run().catch(err => {
+      console.error(err);
+    });
   }
 }
